8870a2c6d0c0b890850e22d895cea652c4cab2d1
Priyamani2004 <manimegalaulm@gmail.com>
2022-12-08 11:34:39 +0530

    commit

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ Array.prototype.cube=function(){
     let b=a.map(y=>y*y*y);
     return b;
   }
-  
+
   String.prototype.Countvowels=function(){
-    let count=0;
+    
   const vowels = ["a", "e", "i", "o", "u"];
+  let count=0;
   let word=this.toLowerCase();
   for(let i=0; i<word.length; i++){
     if(vowels.includes(word[i])){
